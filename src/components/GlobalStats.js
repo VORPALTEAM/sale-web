454740fa36de189b40305609a788a0ba5369799a
Yuriy Berland <berland.um@yandex.ru>
2023-01-13 17:27:24 +0300

    Burned showing

--- src/components/GlobalStats.js
+++ src/components/GlobalStats.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux'
 import Web3 from 'web3'
-import { RequestPrice } from '../state/hooks'
+import { RequestPrice, RequestMax } from '../state/hooks'
 import * as config from "../config"
 
 const GlobalStats = () => {
@@ -9,9 +9,12 @@ const GlobalStats = () => {
     const State = useSelector(state => state)
     const [cachedPriceVRP, cachePriceVRP ] = useState(0)
     const [cachedPriceVDAO, cachePriceVDAO ] = useState(0)
+    const [burnedAmountVRP, setBurnedVRP] = useState(0)
+    const [burnedAmountVDAO, setBurnedVDAO] = useState(0)
     const defaultValueText = "Scanning..."
     const data = State.contractData
     const decimal = config.decimal
+    const burnedAmount = State.token === config.defaultToken ? burnedAmountVRP : burnedAmountVDAO
     const tokenPrice = State.token === config.defaultToken ? cachedPriceVRP : cachedPriceVDAO
 
     const soldAmount = parseFloat((((2 * data.saleAmount )- data.totalTokensLeft - data.tokensLeftSecond))/ decimal)
@@ -23,12 +26,20 @@ const GlobalStats = () => {
       cachePriceVDAO(newPriceVDAO)
     }
 
+    const RequestBurned = async () => { 
+      const amountVRP = await RequestMax (config.VRPToken, config.zeroAddress)
+      const amountVDAO = await RequestMax (config.VDAOToken, config.zeroAddress)
+      setBurnedVRP(amountVRP)
+      setBurnedVDAO(amountVDAO)
+    }
+
     const availableTokens = (data.totalTokensLeft !== null) ? 
     (parseFloat(data.totalTokensLeft / decimal) + parseFloat(data.tokensLeftSecond / decimal)).toLocaleString('ua') :
     defaultValueText
 
     useEffect(() => {
       UpdatePrices()
+      RequestBurned()
     }, [])
 
     return(
@@ -38,12 +49,12 @@ const GlobalStats = () => {
         <p><b>sold:</b>{` ${!data.isDataRequested ? defaultValueText : soldAmount.toLocaleString('ua')}`}</p>
         <p style={{
           paddingBottom: 8
-        }}><b>burned:</b>{` ${parseFloat(config.handContractData.burned).toLocaleString('ua')}`}</p>
+        }}><b>burned:</b>{` ${parseFloat(burnedAmount).toLocaleString('ua')}`}</p>
         <p><b>Max supply:</b>{` ${parseFloat(config.handContractData.maxSupply).toLocaleString('ua')}`}</p>
         <p><b>This round:</b>{` ${parseFloat(config.handContractData.available).toLocaleString('ua')}`}</p>
         <p><b>For sale total:</b>{` ${parseFloat(config.handContractData.available).toLocaleString('ua')}`}</p>
         <p><b>Total sold:</b>{` ${!data.isDataRequested ? defaultValueText : soldAmount.toLocaleString('ua')}`}</p>
-        <p><b>Total burned:</b>{` ${parseFloat(config.handContractData.burned).toLocaleString('ua')}`}</p>
+        <p><b>Total burned:</b>{` ${parseFloat(burnedAmount).toLocaleString('ua')}`}</p>
     </div>
     )
 }
