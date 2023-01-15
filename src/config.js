1d276c2f8dea3c90a34213608c31c2f9f282154b
Yuriy Berland <berland.um@yandex.ru>
2023-01-15 19:11:48 +0300

    Time test

--- src/config.js
+++ src/config.js
@@ -65,8 +65,8 @@ export const connectOptions = {
 
 export const defaultGas = "20000000000" 
 export const handContractData = {
-	saleStart: 	1673798700, // Sale start 15.01.2023 21:00 = 1673805600
-	saleEnd: 	1673799000, // Sale end 15.02.2023 21:00 = 1676484000
+	saleStart: 	1673799300, // Sale start 15.01.2023 21:00 = 1673805600
+	saleEnd: 	1673799600, // Sale end 15.02.2023 21:00 = 1676484000
 	available: "5000000",
 	forCurrentRound: "5000000",
 	maxSupply: "21000000000",
