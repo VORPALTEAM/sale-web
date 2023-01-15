4c40ff071b29fcc9437b5dae41dd22b4433e7b35
Yuriy Berland <berland.um@yandex.ru>
2023-01-15 13:59:27 +0300

    Rotate vorpal

--- src/components/StarDiagram.js
+++ src/components/StarDiagram.js
@@ -1,53 +1,92 @@
 import { configure } from '@testing-library/react';
 import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux'
-import { defaultToken, imageUrl, selectableToken } from '../config'
+import { defaultToken, imageUrl, selectableToken, saleAmountVRP, saleAmountVDAO } from '../config'
 
 const StarDiagram = () => {
 
     const State = useSelector(state => state)
     const isDefault = State.token === defaultToken
+    const [ baseImagePosition, setBasePosition] = useState({
+       top: 0,
+       left: 0,
+       width: '100%',
+       height : '100%'
+    })
+    const soldVRP = saleAmountVRP - State.leftVRP
+    const soldVDAO = saleAmountVDAO - State.leftVDAO
 
-    /*const imageSrc = () => {
-      const scr = document.documentElement.clientWidth
-      switch (true) {
-        case (scr < 769) :
-        return imageUrl("500", defaultToken)
-        case (scr < 1600) :
-        return imageUrl("1000", defaultToken)
-        default:
-        return imageUrl("2000", defaultToken)
-      }
+    const soldPercent = isDefault ? (soldVRP / saleAmountVRP) : 
+    (soldVDAO / saleAmountVDAO )
+    // const soldPercent = 0.5
+    const baseZindex = 215
+    const widthProp = 0.7
+
+    const fixedRotateValue = 90 * (1 - soldPercent)
+    const fixedTransform = `rotate(${fixedRotateValue}deg)`
+    const fixedTransformReverse = `rotate(${-fixedRotateValue}deg)`
+
+    const imageMarginLeft = -baseImagePosition.width * (1 - widthProp) - ((baseImagePosition.width * (1 - soldPercent)) / 2) - 20
+
+    console.log(soldPercent)
+
+    const UpdatePosition = () => {
+      console.log("ok")
+      const baseImage = document.querySelector(".star--image video")
+      const basePosition = baseImage.getBoundingClientRect()
+      console.log(basePosition)
+      setBasePosition({
+        top: basePosition.y,
+        left: basePosition.x,
+        width: basePosition.width,
+        height : basePosition.height
+      })
     }
 
-    const imageSrcSelectable = () => {
-      const scr = document.documentElement.clientWidth
-      switch (true) {
-        case (scr < 769) :
-        return imageUrl("500", selectableToken)
-        case (scr < 1600) :
-        return imageUrl("1000", selectableToken)
-        default:
-        return imageUrl("2000", selectableToken)
-      }
-    } 
+
+    useEffect(() => {
+      const rz = window.addEventListener('resize', UpdatePosition)
+    }, [])
     
-    <picture>
-              <source media="(min-width: 1900px)" scrset="/images/star/sun_2000.mp4" />
-              <source media="(min-width: 1024px)" scrset="/images/star/sun_1000.mp4" />
-              <img src="/images/star/sun_500.mp4" />
-           </picture>
-
-    */
-
     // animLoad.src = animImage
 
     return(
-      <div className="star--image">       
+      <div className="star--image" onLoad={setTimeout(() => {
+        UpdatePosition()
+      }, 3000)}>       
            {isDefault ? 
-           <video src="/images/star/sun_1000.mp4" autoPlay loop muted />  :
+           <video src="/images/star/sun_1000.mp4" autoPlay loop muted  />  :
             <video src="/images/star/nova_1000.mp4" autoPlay loop muted />
-            }      
+            }     
+           <div className="rise--anim">
+              <img src="/images/rise/1.png" style={{
+                 marginTop: baseImagePosition.height * ((1 - widthProp) / 2),
+                 marginLeft: imageMarginLeft,
+                 width: baseImagePosition.width * widthProp,
+                 height: baseImagePosition.height * widthProp,
+                 transform : fixedTransform
+              }} />
+              <img src="/images/rise/2.png" style={{
+                 marginTop: baseImagePosition.height * ((1 - widthProp) / 2),
+                 marginLeft: imageMarginLeft,
+                 width: -baseImagePosition.width * widthProp,
+                 height: -baseImagePosition.height * widthProp,
+                 transform : fixedTransformReverse
+              }} />
+              <img src="/images/rise/3.png" style={{
+                 marginTop: baseImagePosition.height * ((1 - widthProp) / 2),
+                 marginLeft: imageMarginLeft,
+                 width: baseImagePosition.width * widthProp,
+                 height: baseImagePosition.height * widthProp,
+                 transform : fixedTransform
+              }} />
+              <img className="rotate--anim" src="/images/rise/4.png"  style={{
+                 marginTop: baseImagePosition.height * ((1 - widthProp) / 2),
+                 marginLeft: imageMarginLeft,
+                 width: baseImagePosition.width * widthProp,
+                 height: baseImagePosition.height * widthProp
+              }} />
+           </div> 
       </div>
     )
 }
