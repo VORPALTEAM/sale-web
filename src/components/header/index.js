b2f759d0ca6737fbc77981c7c8be5b9a900db3ed
Yuriy Berland <berland.um@yandex.ru>
2023-01-17 10:58:58 +0300

    Menu fix

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -3,45 +3,45 @@ import ConnectWalletBtn from './ConnectWallet';
 import { menu, styles } from './MenuConfig'
 import { mainHost } from '../../config'
 
-const Header = () => {
+const MenuSection = ({ isMobile = false}) => {
+  return(
+    <div className={isMobile ? "menu--section mobile--menu--ctnr" : "menu--section"}>
+    {menu.map((item ,index) => {
+      const hasSubs = item.submenu.length > 0 ? true : false
+      let itemClass = "menu--item"
+      if (hasSubs) {
+        itemClass += " has--submenu"
+      }
+      if (item.style === styles.starmap) {
+        itemClass += " starmap--btn"
+      }
+      if (item.style === styles.sale) {
+        itemClass += " green--btn"
+      }
 
-    const MenuSection = ({ isMobile = false}) => {
       return(
-        <div className={isMobile ? "menu--section mobile--menu--ctnr" : "menu--section"}>
-        {menu.map((item ,index) => {
-          const hasSubs = item.submenu.length > 0 ? true : false
-          let itemClass = "menu--item"
-          if (hasSubs) {
-            itemClass += " has--submenu"
-          }
-          if (item.style === styles.starmap) {
-            itemClass += " starmap--btn"
-          }
-          if (item.style === styles.sale) {
-            itemClass += " green--btn"
-          }
-
-          return(
-            <div className={itemClass} key={"mainmenu"+index}>
-             <a href={item.url} target="_blank">{item.name}</a>
-             {hasSubs ? <div key={"submenu_"+index} className={`main--submenu${isMobile ? " mobile--sub" : ""}`}>
-                {item.submenu.map((subitem, ind) => {
-                   const siClass = `submenu--item${(ind === 0) ? 
-                  " submenu--item--first" :((ind === item.submenu.length - 1) ?
-                " submenu--item--last" : "")}`
-                   return(
-                    <div key={"subs_"+index+"_"+ind} className={siClass}>
-                       <a href={subitem.url} target="_blank">{subitem.name}</a>
-                    </div>
-                   )
-                })} 
-             </div> : null}
-        </div>
-          )
-        })}
-      </div>
+        <div className={itemClass} key={"mainmenu"+index}>
+         <a href={item.url} target="_blank">{item.name}</a>
+         {hasSubs ? <div key={"submenu_"+index} className={`main--submenu${isMobile ? " mobile--sub" : ""}`}>
+            {item.submenu.map((subitem, ind) => {
+               const siClass = `submenu--item${(ind === 0) ? 
+              " submenu--item--first" :((ind === item.submenu.length - 1) ?
+            " submenu--item--last" : "")}`
+               return(
+                <div key={"subs_"+index+"_"+ind} className={siClass}>
+                   <a href={subitem.url} target="_blank">{subitem.name}</a>
+                </div>
+               )
+            })} 
+         </div> : null}
+    </div>
       )
-    }
+    })}
+  </div>
+  )
+}
+
+const Header = () => {
 
 
     return(
