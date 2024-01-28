a07c735c4c82a8e891a1a424a86ab7961c08c40b
Yuriy <berlandspw@gmail.com>
2024-01-28 23:02:57 +0500

    layout

---
+++ src/page/components/cabinet/calcForm.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CalculationForm = () => {
+  return (
+    <div className="calculationForm">
+      <div className="calculationFormInner">Form</div>
+    </div>
+  );
+};
+
+export default CalculationForm;
--- src/page/components/cabinet/purchaseColumn.tsx
+++ src/page/components/cabinet/purchaseColumn.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import DataHeading from "./dataHeading";
+import CalculationForm from "./calcForm";
 
 const PurchaseColumn = () => {
 
     return(
-        <div className="officeColumn balanceColumn">Buy</div>
+        <div className="officeColumn balanceColumn">
+            <DataHeading h="GET YOUR TOKEN" />
+            <CalculationForm />
+            </div>
     )
 }
 
